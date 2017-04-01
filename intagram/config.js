471139e6f34e10584e2de953348c4f44ccf9842c
andrew <lagovskiy@gmail.com>
2017-03-31 21:09:20 -0400

    Set configuration variables

---
+++ intagram/config.js
@@ -0,0 +1,8 @@
+var config = {
+	lob_api_key           : process.env.LOB,
+	intagram_client_id    : process.env.INSTAGRAM_CLIENT_ID,
+	intagram_client_secret: process.env.INSTAGRAM_CLIENT_SECRET,
+	intagram_redirect_uri : process.env.REDIRECT_URI
+}
+
+module.exports = config;
